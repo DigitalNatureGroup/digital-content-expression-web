5ac8c562d9bdbc280cccecbb04473d11d44db5b3
kenkenissocool <s2010246@s.tsukuba.ac.jp>
2022-12-11 20:45:13 +0900

    added forms

--- 2022/js/btn.js
+++ 2022/js/btn.js
@@ -4,3 +4,17 @@ $(function(){
     return false;
   });
 });
+
+$(window).scroll(function () {
+  var scrollAnimationElm = document.querySelectorAll('.forms');
+  var scrollAnimationFunc = function () {
+    for (var i = 0; i < scrollAnimationElm.length; i++) {
+      var triggerMargin = 100;
+      if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
+        scrollAnimationElm[i].classList.add('on');
+      }
+    }
+  }
+  window.addEventListener('load', scrollAnimationFunc);
+  window.addEventListener('scroll', scrollAnimationFunc);
+});
